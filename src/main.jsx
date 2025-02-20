94e75ecf2e07b77aaa10e51d0cee5ac753c84f36
SrBaliardo <tao.john007@icloud.com>
2025-02-20 16:24:49 -0300

    update code-4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles/globalStyles.js'
+import GlobalStyles from './styles/globalStyles'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './routes'
 
